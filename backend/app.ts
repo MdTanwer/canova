8ec6ac9700bc151fd7667233e30d1d60edae6db8
MdTanwer <tanw9004167@gmail.com>
2025-07-22 19:01:46 +0530

    E

--- backend/app.ts
+++ backend/app.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend URL
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
