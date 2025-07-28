ecf543aab995200818c98e5e3bb0371730c71c76
MdTanwer <tanw9004167@gmail.com>
2025-07-28 16:23:58 +0530

    long and short work is done

--- backend/routers/questionroutes.ts
+++ backend/routers/questionroutes.ts
@@ -1,9 +1,33 @@
 import express from "express";
 import {
   createQuestion,
+  reorderQuestions,
+  deleteQuestion,
+  updateQuestion,
+  getQuestionById,
+  getQuestionsByPage,
   uploadReferenceMedia,
+  bulkCreateQuestions,
 } from "../controllesr/formbuilderQuestionController";
 
 export const questionRouter = express.Router();
 
 questionRouter.post("/", uploadReferenceMedia, createQuestion);
+
+// Get questions by page
+questionRouter.get("/:pageId", getQuestionsByPage);
+
+// Get single question
+questionRouter.get("/:questionId", getQuestionById);
+
+// Update question (with optional reference media upload)
+questionRouter.put("/:questionId", uploadReferenceMedia, updateQuestion);
+
+// Delete question
+questionRouter.delete("/:questionId", deleteQuestion);
+
+// Reorder questions within a page
+questionRouter.put("/page/:pageId/reorder", reorderQuestions);
+
+// Bulk create questions
+questionRouter.post("/bulk", bulkCreateQuestions);
